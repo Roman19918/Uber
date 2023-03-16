df965f1f30eda2184e090d793374ddfe7bb7575e
Roman Pryples <Roaman19918@gmail.com>
2023-03-16 22:22:41 +0200

    create burger menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__menu');
+    const menuCloseItem = document.querySelector('.header__menu-close')
+
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__menu_active');
+    });
+
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__menu_active');
+    })
+}());
