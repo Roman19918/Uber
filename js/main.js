8aa93cb13a7f26f8c1c769c372fb7c8070a0c795
Roman Pryples <Roaman19918@gmail.com>
2023-03-22 21:15:51 +0200

    animated section Header, Subheader, Intro, Advantages, Mobile-app

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 //Burger handler
 
 (function () {
-    const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header__menu');
-    const menuCloseItem = document.querySelector('.header__menu-close');
+	const burgerItem = document.querySelector(".burger");
+	const menu = document.querySelector(".header__menu");
+	const menuCloseItem = document.querySelector(".header__menu-close");
 
+	burgerItem.addEventListener("click", () => {
+		menu.classList.add("header__menu_active");
+	});
 
-    burgerItem.addEventListener('click', () => {
-        menu.classList.add('header__menu_active');
-    });
+	menuCloseItem.addEventListener("click", () => {
+		menu.classList.remove("header__menu_active");
+	});
+})();
 
-    menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header__menu_active');
-    });
-}());
+function onEntry(entry) {
+	entry.forEach((change) => {
+		if (change.isIntersecting) {
+			change.target.classList.add("element-show");
+		} else {
+			change.target.classList.remove("element-show");
+		}
+	});
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+	observer.observe(elm);
+}
